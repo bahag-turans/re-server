aa2023d25fc617988bdf36f756161840a07e31f5
bahag-jasharidr <drilon.jashari@bahag.com>
2023-11-02 09:09:11 +0100

    Testing Routes

--- routes.py
+++ routes.py
@@ -2,16 +2,22 @@ from flask_restful import Resource
 from repository import Repository
 from flask import request
 
-repo = Repository()
+repository = Repository()
 
 class EventList(Resource):
+    def __init__(self, repo=repository):
+        self.repo = repo
+        
     def get(self):
-        return [event.__dict__ for event in repo.events_get_all()]
+        return [event.__dict__ for event in self.repo.events_get_all()]
     
 class Event(Resource):
+    def __init__(self, repo=repository):
+        self.repo = repo
+        
     def get(self, event_id):
-        return repo.event_get_by_id(int(event_id)).__dict__
+        return self.repo.event_get_by_id(int(event_id)).__dict__
     
-    def post(self):
-        data = request.get_json()
-        return repo.event_add(data).__dict__
+    def post(self,req=request):
+        data = req.get_json()
+        return self.repo.event_add(data).__dict__
