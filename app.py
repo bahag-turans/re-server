a5b8620b757eef992b73fe805f9eb6f6fef5a57f
bahag-turans <sarper.turan@bahag.com>
2023-11-13 14:19:39 +0100

    IDE changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from routes import EventList, Event
 from flask_cors import CORS
 from psycopg2 import pool
+
 # from dotenv import load_dotenv
 
 # load_dotenv() 
@@ -15,15 +16,13 @@ DB_PORT = os.environ.get('DB_PORT')
 USER = os.environ.get('USER')
 PASSWORD = os.environ.get('PASSWORD')
 MIN = os.environ.get('MIN')
-MAX =  os.environ.get('MAX')
-print("PPPPPPPPPPPPPPPPPPPPPPPPPPP")
-print(HOST)
+MAX = os.environ.get('MAX')
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['pSQL_pool'] = pool.SimpleConnectionPool(MIN,MAX,user=USER,password=PASSWORD,
-                                                    host=HOST,port=DB_PORT,database=DATABASE)
+app.config['pSQL_pool'] = pool.SimpleConnectionPool(MIN, MAX, user=USER, password=PASSWORD,
+                                                    host=HOST, port=DB_PORT, database=DATABASE)
 
 BASE_URL = '/api'
 
@@ -31,9 +30,11 @@ api = Api(app)
 api.add_resource(EventList, f'{BASE_URL}/events')
 api.add_resource(Event, f'{BASE_URL}/event/<event_id>')
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, Flask World!'
+
 
 @app.teardown_appcontext
 def close_conn(e):
@@ -42,7 +43,6 @@ def close_conn(e):
         app.config['pSQL_pool'].putconn(db)
         print("Connection returned to the pool")
 
-if __name__ == '__main__':
-            app.run(debug=True)
 
-            
+if __name__ == '__main__':
+    app.run(debug=True)
--- routes.py
+++ routes.py
@@ -4,29 +4,29 @@ from flask import request
 
 repository = Repository()
 
+
 class EventList(Resource):
     def __init__(self, repo=repository):
         self.repo = repo
-        
+
     def get(self):
         return [event.__dict__ for event in self.repo.events_get_all()]
-    
-    def post(self,req=request):
+
+    def post(self, req=request):
         data = req.get_json()
         return self.repo.event_add(data).__dict__
-    
+
+
 class Event(Resource):
     def __init__(self, repo=repository):
         self.repo = repo
-        
+
     def get(self, event_id):
         return self.repo.event_get_by_id(int(event_id)).__dict__
-    
+
     def delete(self, event_id):
         return self.repo.event_delete(int(event_id))
-    
-    def put(self,event_id, req=request):
+
+    def put(self, event_id, req=request):
         data = req.get_json()
-        print("data in server", data)
         return self.repo.event_update(data).__dict__
-    
